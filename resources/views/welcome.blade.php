5b668ab76c5040c79eaf0e72663336eefef70417
emxmax <emxmax@hotmail.com>
2021-12-26 21:31:52 -0500

    update welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,11 +16,29 @@
             }
         </style>
     </head>
-    <body class="container" style="min-height: 100vh">
-        <div class="row d-flex justify-content-center align-items-center"  style="min-height: 100vh">
-            <a class="btn btn-success" style="font-size: 2rem"
-               href="{{route('login')}}">Log in with Spotify
-            </a>
+    <body class="container">
+        <div style="padding-top:30vh;">
+            <div class="row justify-content-center">
+                <label for="terminos" style="width: 250px;line-height:18px;margin-bottom:20px;text-align:center;"><input id="terminos" type="checkbox"> Permite usted que la aplicación pueda acceder a su lista de géneros musicales de Spotify.</label>
+            </div>
+            <div class="row d-flex justify-content-center align-items-center">
+                <a id="btnlogin" class="btn btn-success disabled" style="font-size: 2rem"
+                href="{{route('login')}}" role="button" aria-disabled="true">Log in with Spotify
+                </a>
+            </div>
         </div>
     </body>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script>
+         $(document).ready(function(){
+            $('#terminos').change(function(){
+                var active = "";
+                if ($(this).is(":checked")){
+                    $('#btnlogin').removeClass('disabled')
+                }else{
+                    $('#btnlogin').addClass('disabled')
+                }
+            });
+         });
+    </script>
 </html>
